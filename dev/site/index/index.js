c471f1a9ea2166b39aa71beb4cd8f6d6e98ef58f
Xiao Shen <x_x@live.com>
2016-07-09 09:01:02 +0800

    no message

--- dev/site/index/index.js
+++ dev/site/index/index.js
@@ -1,4 +1,22 @@
 'use strict';
 define(['common/kernel/kernel'], function(kernel) {
-	kernel.init();
+	//百度统计代码
+	if (location.host === 'your_production_host') {
+		window._hmt = [
+			['_setAutoPageview', false]
+		];
+		require(['//hm.baidu.com/hm.js?[your_hmid]'], function(){
+			//由于百度统计在head中插入的input标签在ie7中会导致jquery选择器遍历时出错，这里尝试将其移除
+			var ipt = head.getElementsByTagName('input')[0];
+			if (ipt) {
+				head.removeChild(ipt);
+			}
+		});
+	}
+	kernel.init('home', undefined, function(){
+		//百度统计接口
+		if (window._hmt && _hmt.push) {
+			_hmt.push(['_trackPageview', '/' + kernel.buildHash(kernel.location)]);
+		}
+	});
 });
--- framework/src/init.js
+++ framework/src/init.js
@@ -1,20 +1,20 @@
 'use strict';
-var _hmt;
 ! function() {
-	var head = document.head || document.getElementsByTagName('head')[0];
+	var prefix = '/fusion/',
+		head = document.head || document.getElementsByTagName('head')[0];
 	if (!window.XMLHttpRequest) {
 		window.onload = function() {
 			document.getElementById('loading').firstChild.firstChild.firstChild.data = '请使用IE7+、Chrome、Firefox访问';
 		};
 	} else {
-		require(['/fusion/framework/require-config.js?' + new Date().valueOf()], function() {
+		require([prefix + 'framework/require-config.js?' + new Date().valueOf()], function() {
 			var l = document.createElement('link');
 			var m = document.createElement('link');
 			if (require.data.debug) {
 				l.rel = m.rel = 'stylesheet/less';
 				l.href = require.toUrl('site/index/index.less');
 				m.href = require.toUrl('common/kernel/kernel.less');
-				require(['/fusion/framework/less.js'], function() {
+				require([prefix + 'framework/less.js'], function() {
 					require(['site/index/index']);
 				});
 			} else {
@@ -26,20 +26,5 @@ var _hmt;
 			head.appendChild(m);
 			head.appendChild(l);
 		});
-		//百度统计代码
-		if (location.host === 'your_production_host') {
-			_hmt = [
-				['_setAutoPageview', false]
-			];
-			require(['//hm.baidu.com/hm.js?[your_hmid]'], function(){
-				//由于百度统计在head中插入的input标签在ie7中会导致jquery选择器遍历时出错，这里尝试将其移除
-				if (/MSIE 7/.test(navigator.userAgent)) {
-					var ipt = head.getElementsByTagName('input')[0];
-					if (ipt) {
-						head.removeChild(ipt);
-					}
-				}
-			});
-		}
 	}
 }();
