691c72166616dfd3cc3e7c1f4353b9d4518df063
xxoo <dead.xx@gmail.com>
2018-09-28 02:04:26 +0800

    no message

--- framework/combine.cmd
+++ framework/combine.cmd
@@ -1,2 +1,2 @@
 cd /d "%~dp0"
-uglifyjs src\es5-shim.js src\es6-shim.js src\jquery.js src/jquery.migrate.js src\require.js src\init.js -c hoist_vars,unsafe,comparisons --support-ie8 -m -o all.js
+terser src\es5-shim.js src\es6-shim.js src\jquery.js src/jquery.migrate.js src\require.js src\init.js -c hoist_vars,unsafe,comparisons --ie8 --safari10 -m -o all.js
