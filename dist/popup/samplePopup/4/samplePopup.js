b86c024698e7512d0fd18eb4dfc921ad49b0809a
xxoo <dead.xx@gmail.com>
2018-09-29 17:38:09 +0800

    no message

--- dist/popup/samplePopup/4/samplePopup.js
+++
@@ -1,1 +0,0 @@
-'use strict';define(['module','common/kernel/kernel'],function(a,b){var c=a.id.replace(/^[^/]+\/|\/[^/]+/g,''),d=$('#'+c),e=d.find('>.content>span');return{open:function d(a){e.text(a),b.showPopup(c)},onunload:function a(){console.log('closing '+c)}}});
